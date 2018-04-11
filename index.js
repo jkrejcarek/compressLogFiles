c59e9b91734091822fa285baed2e8eed7aef0c37
Jan Krejcarek <jan.krejcarek@gmail.com>
2018-04-11 14:12:00 +0200

    Reformating

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 let winston = require('winston');
 
 var logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.Console)({'level':'verbose'}),
-      new (winston.transports.File)({'level':'verbose', 'filename':'compress.log', 'maxsize':1024 * 1024, 'zippedArchive':true, 'json':false})
-    ]
+  transports: [
+    new (winston.transports.Console)({ 'level': 'verbose' }),
+    new (winston.transports.File)({ 'level': 'verbose', 'filename': 'compress.log', 'maxsize': 1024 * 1024, 'zippedArchive': true, 'json': false })
+  ]
 });
 
 logger.cli();
 
 const dirName = process.argv[2];
 if (!dirName) {
-    throw Error('Directory must be specified');
+  throw Error('Directory must be specified');
 }
 
 const fs = require('fs');
@@ -21,7 +21,7 @@ const path = require('path');
 
 let dirStat = fs.statSync(dirName);
 if (!dirStat.isDirectory()) {
-    throw Error(dirName + " must be a directory");
+  throw Error(dirName + " must be a directory");
 }
 
 const zlib = require('zlib');
@@ -32,45 +32,45 @@ let dateString = moment().format('MMDDYYYY');
 
 fs.readdir(dirName, (err, files) => {
 
-    if (err) {        
-      logger.error(err);
-    }
+  if (err) {
+    logger.error(err);
+  }
 
-    for (let file of files) { 
-        let fullpath = dirName.endsWith(path.sep) ? dirName + file : dirName + path.sep + file;
+  for (let file of files) {
+    let fullpath = dirName.endsWith(path.sep) ? dirName + file : dirName + path.sep + file;
 
-        let stat = fs.statSync(fullpath);
-        if (stat.isFile() && path.extname(fullpath) == '.log') {
-          let baseFileName = path.basename(fullpath, '.log');
-          if (!baseFileName.endsWith(dateString)) {
-            logger.info("compressing " + fullpath);
-                        
-            try {
-                const gzip = zlib.createGzip();
-                const reader = fs.createReadStream(fullpath);
-                const writer = fs.createWriteStream(fullpath + ".gz");
+    let stat = fs.statSync(fullpath);
+    if (stat.isFile() && path.extname(fullpath) == '.log') {
+      let baseFileName = path.basename(fullpath, '.log');
+      if (!baseFileName.endsWith(dateString)) {
+        logger.info("compressing " + fullpath);
 
-                writer.on('close', () => {
-                    logger.verbose("GZip file closed, deleting original " + fullpath);
-                    fs.unlinkSync(fullpath);
-                    remainingCount--;
+        try {
+          const gzip = zlib.createGzip();
+          const reader = fs.createReadStream(fullpath);
+          const writer = fs.createWriteStream(fullpath + ".gz");
 
-                    if (remainingCount == 0) {
-                      logger.info("Done zipping all files");
-                    } else {
-                      logger.verbose("Remains: " + remainingCount);
-                    }
-                });
+          writer.on('close', () => {
+            logger.verbose("GZip file closed, deleting original " + fullpath);
+            fs.unlinkSync(fullpath);
+            remainingCount--;
 
-                remainingCount++;
-                reader.pipe(gzip).pipe(writer);
-
-            } catch (err) {
-                logger.error("Error compressing and removing a fullpath: " + err);
+            if (remainingCount == 0) {
+              logger.info("Done zipping all files");
+            } else {
+              logger.verbose("Remains: " + remainingCount);
             }
-          } else {
-            logger.info('Skipping today file ' + fullpath);
-          }
+          });
+
+          remainingCount++;
+          reader.pipe(gzip).pipe(writer);
+
+        } catch (err) {
+          logger.error("Error compressing and removing a fullpath: " + err);
         }
+      } else {
+        logger.info('Skipping today file ' + fullpath);
+      }
     }
+  }
 });
